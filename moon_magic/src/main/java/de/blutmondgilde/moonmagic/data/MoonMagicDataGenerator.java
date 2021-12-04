7d81408435e759de92779bae61d21e3ba7fb2b8a
Christian Raoulis <craoulis@googlemail.com>
2021-12-04 21:52:04 +0100

    fix datagenerator won't be called on runData

--- moon_magic/src/main/java/de/blutmondgilde/moonmagic/data/MoonMagicDataGenerator.java
+++ moon_magic/src/main/java/de/blutmondgilde/moonmagic/data/MoonMagicDataGenerator.java
@@ -1,12 +1,10 @@
 package de.blutmondgilde.moonmagic.data;
 
-import de.blutmondgilde.moonmagic.MoonMagic;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 
 public class MoonMagicDataGenerator {
-    public static void onGatherData(GatherDataEvent e) {
-        MoonMagic.getLogger().info("Generate Data...");
+    public static void onGatherData(final GatherDataEvent e) {
         DataGenerator generator = e.getGenerator();
         generator.addProvider(new MoonMagicLootTableProvider(generator));
     }
