b0f089b6baf7166b503333061a04a145bcf099dd
Christian Raoulis <craoulis@googlemail.com>
2021-12-05 10:01:40 +0100

    set creative tab icons to correct itemstacks

--- moon_magic/src/main/java/de/blutmondgilde/moonmagic/registry/MoonMagicCreativeTabs.java
+++ moon_magic/src/main/java/de/blutmondgilde/moonmagic/registry/MoonMagicCreativeTabs.java
@@ -1,20 +1,21 @@
 package de.blutmondgilde.moonmagic.registry;
 
+import de.blutmondgilde.moonmagic.block.MoonMagicBlocks;
+import de.blutmondgilde.moonmagic.item.MoonMagicItems;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 
 public class MoonMagicCreativeTabs {
     public static final CreativeModeTab ITEM_CREATIVE_TAB = new CreativeModeTab("moon_magic_items") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(Items.IRON_INGOT);
+            return new ItemStack(MoonMagicItems.PURPLE_CRYSTAL_SHARD);
         }
     };
     public static final CreativeModeTab BLOCK_CREATIVE_TAB = new CreativeModeTab("moon_magic_blocks") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(Items.IRON_BLOCK);
+            return new ItemStack(MoonMagicBlocks.BLUE_CAVE_CRYSTAL.asItem());
         }
     };
 }
